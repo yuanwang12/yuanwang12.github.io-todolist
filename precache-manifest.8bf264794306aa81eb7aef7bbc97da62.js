f6bd5d68abf458f70b8361dee9bc7cba44a4437b
25801 <yuan.wang07@hand-china.com>
2019-07-24 22:32:57 +0800

    Updates

---
+++ precache-manifest.8bf264794306aa81eb7aef7bbc97da62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0af25f131b8c1aee934b91130f2e93d",
+    "url": "/yuanwang12.github.io-todolist/index.html"
+  },
+  {
+    "revision": "8cfda010868e6098ab7f",
+    "url": "/yuanwang12.github.io-todolist/static/css/main.7d198186.chunk.css"
+  },
+  {
+    "revision": "342b763149df18a11f74",
+    "url": "/yuanwang12.github.io-todolist/static/js/2.374d8ee3.chunk.js"
+  },
+  {
+    "revision": "8cfda010868e6098ab7f",
+    "url": "/yuanwang12.github.io-todolist/static/js/main.58979c80.chunk.js"
+  },
+  {
+    "revision": "9d2c6874a0ea8ae1e473",
+    "url": "/yuanwang12.github.io-todolist/static/js/runtime~main.da6c84c9.js"
+  }
+]);
